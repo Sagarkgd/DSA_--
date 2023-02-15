9cd3c4b46ff8ec5ffc648cc76dbf0e7e7a6ca8bd
Sagarkgd <119275572+Sagarkgd@users.noreply.github.com>
2023-02-15 14:04:22 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -1,39 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"time"
-)
+import "fmt"
 
 func main() {
-	links := []string{ //slice of string having links.
-		"http://google.com",
-		"http://facebook.com",
-		"http://stackoverflow.com",
-		"http://golang.org",
-		"http://amazon.com",
-	}
-	c := make(chan string) //creating a channel of type string,using make keyword...
-	for _, link := range links {
-		go checkLink(link, c)
-	}
-	for l := range c {
-		go func(link string) {
-			time.Sleep(5 * time.Second)
-			checkLink(link, c)
-		}(l)
-	}
+	number := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-}	
-
-func checkLink(link string, c chan string) { //func taking link string and channel of string type.
-	_, err := http.Get(link) //only checking if there is error.
-	if err != nil {
-		fmt.Println(link, "not working..") //printing error
-		c <- link                          //passing error message to channel.
-		return                             //if error occur return.
+	for _, value := range number {
+		if value%2 == 0 {
+			fmt.Printf("%d is even \n", value)
+		} else {
+			fmt.Printf("%d is odd \n", value)
+		}
 	}
-	fmt.Println(link, "yes its working ") //if error doea not occurs
-	c <- link                             //passing string to channel.
 }
